53f54a93ec5ee0d66dde14b3c966ba705ab48cb3
Aman Yadav <91108730+Ydvaaman@users.noreply.github.com>
2022-12-05 23:56:22 +0530

    Time: 1 ms (53.00%), Space: 45.8 MB (75.26%) - LeetHub

---
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if(root.left==null && root.right==null){
+            if(root.val==0){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        if(root.val==2){
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        }
+        if(root.val==3){
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+        return false;
+    }
+}
