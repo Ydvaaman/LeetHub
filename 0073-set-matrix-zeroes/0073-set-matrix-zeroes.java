698c8a974fece8296cb38cff8477bdec758f24ea
Aman Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-06-27 06:52:42 -0700

    Time: 2 ms (21.79%), Space: 44.5 MB (64.28%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,5 +1,6 @@
 class Solution {
     
+    // making rows negative excluding 0's
     public static void makeRow(int matrix[][],int n,int m,int i){
         for(int j=0;j<n;j++){
             if(matrix[i][j]!=0){
@@ -9,6 +10,7 @@ class Solution {
         }
     }
     
+      // making cols negative excluding 0's
     public static void makeCol(int matrix[][],int n,int m,int j){
         for(int i=0;i<m;i++){
             if(matrix[i][j]!=0){
@@ -17,11 +19,11 @@ class Solution {
         }
     }
     
-    
+    // main function
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
         int n = matrix[0].length;
-        
+        int m = matrix.length;
+       
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
@@ -30,6 +32,8 @@ class Solution {
                 }
             }
         }
+        
+        // itterate through the matrix and mark 0's where negative is present 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]== -10000000){
