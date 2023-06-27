0df06621b19c339c19a8f889dc91c46cbc3478ec
Aman Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-06-27 07:27:17 -0700

    Time: 0 ms (100.00%), Space: 44.6 MB (52.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,46 +1,28 @@
 class Solution {
-    
-    // making rows negative excluding 0's
-    public static void makeRow(int matrix[][],int n,int m,int i){
-        for(int j=0;j<n;j++){
-            if(matrix[i][j]!=0){
-                matrix[i][j] = -10000000;
-            }
-            
-        }
-    }
-    
-      // making cols negative excluding 0's
-    public static void makeCol(int matrix[][],int n,int m,int j){
-        for(int i=0;i<m;i++){
-            if(matrix[i][j]!=0){
-                matrix[i][j] = -10000000;
-            }
-        }
-    }
-    
-    // main function
     public void setZeroes(int[][] matrix) {
+        
         int n = matrix[0].length;
         int m = matrix.length;
-       
+        
+        int row[] = new int[m];
+        int col[] = new int[n];
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    makeRow(matrix,n,m,i);
-                    makeCol(matrix,n,m,j);
+                if(matrix[i][j] == 0){
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
         
-        // itterate through the matrix and mark 0's where negative is present 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(matrix[i][j]== -10000000){
-                    matrix[i][j]=0;
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j] = 0;
                 }
-                
             }
         }
+        
     }
 }
