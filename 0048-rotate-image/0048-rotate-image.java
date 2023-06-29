236f7106505816a4232e89ff0713819091ad9560
Aman Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-06-29 13:00:10 -0700

    Time: 0 ms (100.00%), Space: 41.3 MB (72.58%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,7 +1,7 @@
 class Solution {
     
-    public static int[][] ans(int matrix[][]){
-         int n = matrix.length;
+    public static int[][] ans(int [][] matrix){
+        int n = matrix.length;
         int rotated[][] = new int [n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -10,29 +10,14 @@ class Solution {
         }
         return rotated;
     }
+    
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-       int rotated[][] = ans(matrix) ;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-              matrix[i][j] = rotated[i][j];
+        int[][] rotated = ans(matrix);
+        for(int i =0;i<n;i++){
+            for(int j =0;j<n;j++){
+                matrix[i][j] = rotated[i][j];
             }
-           
         }
     }
 }
-
-//  int n = matrix.length;
-//     int [][] ans = new int[n][n] ;
-    
-//     for(int i = 0 ; i < matrix.length ; i++){
-//         for(int j = 0 ; j< matrix.length ; j++){
-//             ans[j][n-1-i] = matrix[i][j];
-//         }
-//     }
-    
-//     for(int i = 0 ; i < matrix.length ; i++){
-//         for(int j = 0 ; j< matrix.length ; j++){
-//             matrix[i][j] = ans[i][j];
-//         }
-//     }
