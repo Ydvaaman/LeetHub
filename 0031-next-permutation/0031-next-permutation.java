7507e37ceb9698ec9479e49224ea2375580cc708
Aman Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-07-05 09:05:59 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (38.99%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -18,10 +18,6 @@ class Solution {
     }
     
     public void reverse(int nums[],int i , int j){
-        while(i<j){
-             swap(nums,i,j);
-            i++;
-            j--;
-        }
+        while(i<j) swap(nums,i++,j--);
     }
 }
