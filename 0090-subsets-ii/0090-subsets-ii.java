396a3673bfe5e3da005f465b2c95e8ce1ed61ce5
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-08-20 03:10:58 +0530

    Time: 6 ms (12.45%), Space: 43.8 MB (62.94%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        Arrays.sort(nums);
+        //start backtracking from the beginning
+        backtrack(resultList, new ArrayList<>(), nums, 0);
+        return resultList;
+    }
+    
+private void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet, int [] nums, int start){
+    // if the set already present then continue
+    if(resultSets.contains(tempSet)) return;
+    
+    resultSets.add(new ArrayList<>(tempSet));
+    
+    for(int i=start;i<nums.length;i++){
+        // case including the number
+        tempSet.add(nums[i]);
+        
+        // backtrack the new subset
+        backtrack(resultSets, tempSet, nums, i+1);
+        
+        tempSet.remove(tempSet.size()-1);
+    }
+}
+}
+    
