d55ac5964a4c9714ffe36d3e9d3b573f6760c69c
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-08-19 17:43:50 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (91.31%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,17 +1,30 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>(); //power Set
-        res.add(new ArrayList());
-        
-        for( int num:nums){
-            int n = res.size();
-            for(int j = 0; j<n; j++){
-                List<Integer> temp = new ArrayList(res.get(j));
-                temp.add(num);
-                res.add(temp);
-            }
+        List<List<Integer>> resultList = new ArrayList<>();
+        backtrack(resultList, new ArrayList<>(), nums, 0);
+        return resultList;
+    }
+    private void backtrack(List<List<Integer>> resultSets, List<Integer> tempSet, int[] nums, int start){
+        resultSets.add(new ArrayList<>(tempSet));
+        for(int i=start;i<nums.length;i++){
+            tempSet.add(nums[i]);
+            backtrack(resultSets, tempSet, nums, i+1);
+            tempSet.remove(tempSet.size()-1);
         }
-        return res;
-        
     }
 }
+
+// IIterative method
+    
+//         List<List<Integer>> res = new ArrayList<>(); //power Set
+//         res.add(new ArrayList());
+        
+//         for( int num:nums){
+//             int n = res.size();
+//             for(int j = 0; j<n; j++){
+//                 List<Integer> temp = new ArrayList(res.get(j));
+//                 temp.add(num);
+//                 res.add(temp);
+//             }
+//         }
+//         return res;
