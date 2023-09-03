2693db6e5c59c40bc9c2e14c38b1d2daf4772085
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-09-04 00:24:03 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (35.31%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.java
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.java
@@ -1,19 +1,9 @@
 class Solution {
+
     public int getSum(int x, int y) {
-   while (y != 0)
-        {
-            // carry now contains common
-            // set bits of x and y
+        while (y != 0) {
             int carry = x & y;
- 
-            // Sum of bits of x and
-            // y where at least one
-            // of the bits is not set
             x = x ^ y;
- 
-            // Carry is shifted by
-            // one so that adding it
-            // to x gives the required sum
             y = carry << 1;
         }
         return x;
