cca28fcc051666cf11e57c52e6d842fc75e2de32
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-09-08 18:45:31 +0530

    Time: 1866 ms (6.87%), Space: 0B (100.00%) - LeetHub

--- 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-select date_id, make_name,
-count(distinct(lead_id)) as unique_leads,
-count(distinct(partner_id)) as unique_partners
-from DailySales
-group by date_id, make_name;
+select date_id, make_name,        /*select the required column name*/
+count(distinct(lead_id)) as unique_leads, /*count the different lead_id value and named the column as unique_leads*/
+count(distinct(partner_id)) as unique_partners  /*count the different partner_id value and named the column as                                                             unique_partners*/
+from DailySales                            /* from the daily sales table*/
+group by date_id, make_name;             /* and group the table into date_id and make_name */
