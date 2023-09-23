2146f6e216efe92b5ac4a55c6aa1a509c5f4beec
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-09-24 01:58:41 +0530

    Time: 1025 ms (87.24%), Space: 0B (100.00%) - LeetHub

--- 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
+++ 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
@@ -1,2 +1,5 @@
-select event_day as day, emp_id , sum(out_time) - sum(in_time) as total_time
+# select event_day as day, emp_id , sum(out_time) - sum(in_time) as total_time
+# from employees group by emp_id,event_day;
+
+select event_day as day, emp_id , sum(out_time-in_time) as total_time
 from employees group by emp_id,event_day;
