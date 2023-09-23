49416c51208d3702f9785f306bd62dfef38315d3
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-09-24 01:19:31 +0530

    Time: 1411 ms (56.03%), Space: 0B (100.00%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,4 +1,5 @@
 # select rating from queries group by rating having count(rating) <3; 
 
-select query_name, round((avg(rating/position)),2) quality, round((sum(case when rating<3 then 1 else 0 end)/count(query_name))*100,2) poor_query_percentage from queries group by query_name;
+# select query_name, round((avg(rating/position)),2) quality, round((sum(case when rating<3 then 1 else 0 end)/count(query_name))*100,2) poor_query_percentage from queries group by query_name;
 
+select query_name, round((avg(rating/position)),2) quality, round((sum(rating<3)/count(query_name))*100,2) poor_query_percentage from queries group by query_name;
