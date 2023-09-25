6e9220a4747b41ec3538b120c7883f0b43326c35
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-09-25 19:50:10 +0530

    Time: 3 ms (45.31%), Space: 40.9 MB (29.39%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,21 +1,21 @@
 class Solution {
+
     public char findTheDifference(String s, String t) {
         char[] str1 = s.toCharArray();
         char[] str2 = t.toCharArray();
         Arrays.sort(str1);
         Arrays.sort(str2);
-        
+
         int s_pointer = 0;
         int t_pointer = 0;
-        
-        while(s_pointer < s.length()){
-            if(str1[s_pointer]!=str2[t_pointer]){
+
+        while (s_pointer < s.length()) {
+            if (str1[s_pointer] != str2[t_pointer]) {
                 return str2[t_pointer];
             }
             s_pointer++;
             t_pointer++;
-            
         }
-        return str2[t.length()-1];
+        return str2[t.length() - 1];
     }
 }
