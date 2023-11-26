e668f1aa9affa5ec2e79bd386448e45043b77154
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2023-11-27 00:01:30 +0530

    Time: 3 ms (22.49%), Space: 39.5 MB (30.14%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int maximum69Number (int num) {
+           String s = Integer.toString(num);
+        s = s.replaceFirst("6","9");
+        return Integer.parseInt(s);
+    }
+}
