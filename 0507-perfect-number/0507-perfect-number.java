4ff6de821101287db0c047e4f275c532c044acee
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2024-01-16 21:12:34 +0530

    Time: 2087 ms (47.29%), Space: 40 MB (49.46%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,12 +1,9 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        List<Integer> list = new ArrayList<>();
-        for(int i=1;i<num;i++){
-            if(num%i==0) list.add(i);
-        }
+     
         int sum =0 ;
-        for(int i=0;i<list.size();i++){
-            sum+=list.get(i);
+        for(int i=1;i<num;i++){
+            if(num%i==0) sum+=i;
         }
         return sum==num;
     }
