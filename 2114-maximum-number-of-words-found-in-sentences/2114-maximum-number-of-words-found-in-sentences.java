f4d1cc5ad1e4a5c976bd194713b4c7724d6f8d5b
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2024-02-25 01:09:14 +0530

    Time: 2 ms (97.41%), Space: 43.4 MB (70.48%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.java
@@ -1,26 +1,35 @@
 class Solution {
     public int mostWordsFound(String[] sentences) {
-        int count = Integer.MIN_VALUE;
-        for(int i=0;i<sentences.length;i++){
-            String str = sentences[i]; 
-            count = Math.max(count, countword(str));
-        }
-        return count;
-    }
-    public int countword(String str)
-    {
-        int count =1;
-         if(str.length() == 0)
-         {
-            return 0;
-         }
-        for(int i=0;i<str.length();i++)
-        {
-            if(str.charAt(i)==' ')
-            {
-                count++;
+        // int count = Integer.MIN_VALUE;  
+    //     for(int i=0;i<sentences.length;i++){ 
+    //         count = Math.max(count, countword(sentences[i]));
+    //     }
+    //     return count;
+    // }
+    // public int countword(String str)
+    // {
+    //     int count =1;
+    //      if(str.length() == 0)
+    //      {
+    //         return 0;
+    //      }
+    //     for(int i=0;i<str.length();i++)
+    //     {
+    //         if(str.charAt(i)==' ')
+    //         {
+    //             count++;
+    //         }
+    //     }
+        int max = Integer.MIN_VALUE;
+        for(String str : sentences){
+            int count = 1;
+            for(char ch : str.toCharArray()){
+                if(ch == ' '){
+                    count++;
+                }
             }
+            max = Math.max(count,max);
         }
-        return count;
+        return max;
     }
 }
