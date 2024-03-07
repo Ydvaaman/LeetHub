49dbfacf2867cf2f3457070ee01049e27dbc8e16
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2024-03-07 15:14:58 +0530

    Time: 3 ms (98.57%), Space: 44.7 MB (66.98%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.java
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -1,25 +1,13 @@
-public class Solution {
-
+class Solution {
     public String finalString(String s) {
-        String str = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'i') {
-                String res = str.substring(0, str.length());
-                str = rev(res);
-            } else{
-                str += s.charAt(i);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == 'i'){
+                sb.reverse();
+            }else{
+                sb = sb.append(s.charAt(i));
             }
         }
-
-        return str;
-    }
-
-    public String rev(String s) {
-        String ans = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            ans += s.charAt(i);
-        }
-        return ans;
+        return sb.toString();
     }
 }
