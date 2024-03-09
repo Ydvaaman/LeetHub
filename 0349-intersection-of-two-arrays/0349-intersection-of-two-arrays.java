4eec95011d7bbcd953401d4e3b22b6f9901c765f
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2024-03-09 15:56:08 +0530

    Time: 2 ms (94.61%), Space: 43.4 MB (36.29%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -7,20 +7,45 @@ class Solution {
         }
         Set<Integer> set2 = new HashSet<>();
         for (int v : nums2) {
-            set2.add(v);
-        }
-        Set<Integer> set = new HashSet<>();
-        for (Integer num : set1) {
-            if (set2.contains(num)) {
-                set.add(num);
+            if(set1.contains(v)){
+                set2.add(v);
             }
         }
-        int[] ans = new int[set.size()];
+        int[] ans = new int[set2.size()];
         int idx = 0;
-        for (Integer val : set) {
+        for(Integer val : set2){
             ans[idx] = val.intValue();
             idx++;
         }
         return ans;
     }
 }
+
+// another way:
+
+// class Solution {
+
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         Set<Integer> set1 = new HashSet<>();
+//         for (int n : nums1) {
+//             set1.add(n);
+//         }
+//         Set<Integer> set2 = new HashSet<>();
+//         for (int v : nums2) {
+//             set2.add(v);
+//         }
+//         Set<Integer> set = new HashSet<>();
+//         for (Integer num : set1) {
+//             if (set2.contains(num)) {
+//                 set.add(num);
+//             }
+//         }
+//         int[] ans = new int[set.size()];
+//         int idx = 0;
+//         for (Integer val : set) {
+//             ans[idx] = val.intValue();
+//             idx++;
+//         }
+//         return ans;
+//     }
+// }
