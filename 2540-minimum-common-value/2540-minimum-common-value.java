3eb3028ddc3f9cbf0965e23cd761906d89a22151
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2024-03-09 10:41:10 +0530

    Time: 21 ms (6.89%), Space: 63.7 MB (21.19%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,20 +1,37 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        int n = nums1.length;
-        int m = nums2.length;
-        int i = 0;
-        int j = 0;
-        while(i<n && j<m){
-            if(nums1[i]!=nums2[j]){
-                if(nums1[i] > nums2[j]){
-                    j++;
-                }else{
-                    i++;
-                }
-            }else if(nums1[i] == nums2[j]){
-                return nums1[i];
+        HashSet<Integer> set = new HashSet<>();
+        for(int n : nums1){
+            set.add(n);
+        }
+        for(int v : nums2){
+            if(set.contains(v)){
+                return v;
             }
         }
         return -1;
     }
 }
+
+// another approach:
+
+// class Solution {
+//     public int getCommon(int[] nums1, int[] nums2) {
+//         int n = nums1.length;
+//         int m = nums2.length;
+//         int i = 0;
+//         int j = 0;
+//         while(i<n && j<m){
+//             if(nums1[i]!=nums2[j]){
+//                 if(nums1[i] > nums2[j]){
+//                     j++;
+//                 }else{
+//                     i++;
+//                 }
+//             }else if(nums1[i] == nums2[j]){
+//                 return nums1[i];
+//             }
+//         }
+//         return -1;
+//     }
+// }
