19bb783fd1dae44be59eca876dbe35180e369de5
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2024-03-11 19:56:41 +0530

    Time: 32 ms (13.67%), Space: 44.1 MB (74.74%) - LeetHub

---
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int countWords(String[] words1, String[] words2) {
+        String[] ans = new String[words1.length + words2.length];
+        int idx = 0;
+        for (String s1 : words1) {
+            ans[idx] = s1;
+            idx++;
+        }
+        for (String s2 : words2) {
+            ans[idx] = s2;
+            idx++;
+        }
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String str : ans) {
+            map.put(str, map.getOrDefault(str, 0) + 1);
+        }
+        int count = 0;
+
+        for (String st : ans) {
+            if (map.get(st) == 2 && Arrays.asList(words1).contains(st) && Arrays.asList(words2).contains(st)) {
+                count++;
+            }
+        }
+        return count/2;
+    }
+}
