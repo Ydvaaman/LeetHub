9a61557b02970647a7971b330c488d97a9d87e48
Aman Kumar Yadav <91108730+Ydvaaman@users.noreply.github.com>
2024-03-23 21:10:57 +0530

    Time: 1 ms (67.59%), Space: 41.2 MB (33.01%) - LeetHub

---
+++ 0504-base-7/0504-base-7.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        if (num < 0) {
+            return "-" + convertToBase7(-num);
+        }
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            sb.append(num % 7);
+            num = num / 7;
+        }
+        return sb.reverse().toString();
+    }
+}
